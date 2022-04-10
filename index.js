143c43ff7ac38233123a4f2c1f40dd4c3419c096
nicolaskiprop <yegonicholasgmail.com>
2022-04-10 23:06:14 +0300

    airdrops

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
-console.log(publicKey);
-console.log(secretKey);
- 
+
+///function to print wallet balance
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`wallet balance is, ${walletBalance}`);
+    } catch(err) {
+        console.error(err);
+    }
+}
+const airDropSol = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        console.log('--------------minting airdrop------------');
+        await connection.confirmTransaction(fromAirDropSignature)
+        
+    } catch (error) {
+        console.log(err);
+    }
+}
+const main = async() => {
+    await getWalletBalance()
+    await airDropSol()
+    await getWalletBalance()
+
+}
+main()
